1e4820139a3176de28d8c86698e8aff460176897
Patricia Selpa <patricia.selpa@gmail.com>
2020-09-28 20:39:11 -0300

    Exercícios 4 a 9

--- ArrayList2.cs
+++ ArrayList2.cs
@@ -43,19 +43,123 @@ namespace second_arrays_list_tests
         }
 
 
-        public (double, double, double, double) Exercise3()
-        {
-            //Dado que a aplicação está preparada. Quando o usuário chamar o exercício 3, então a aplicação deverá receber o sexo do entrevistado 
-            //e a sua resposta (sim ou não). Sabendo-se que foram entrevistadas 10 pessoas, fazer um algoritmo que calcule e escreva:
-            //O número de pessoas que responderam sim;
-            //O número de pessoas que responderam não;
-            //A percentagem de pessoas do sexo feminino que responderam sim;
-            //A percentagem de pessoas do sexo masculino que responderam não;
+        // public (double, double, double, double) Exercise3()
+        // {
+        //     //Dado que a aplicação está preparada. Quando o usuário chamar o exercício 3, então a aplicação deverá receber o sexo do entrevistado 
+        //     //e a sua resposta (sim ou não). Sabendo-se que foram entrevistadas 10 pessoas, fazer um algoritmo que calcule e escreva:
+        //     //O número de pessoas que responderam sim;
+        //     //O número de pessoas que responderam não;
+        //     //A percentagem de pessoas do sexo feminino que responderam sim;
+        //     //A percentagem de pessoas do sexo masculino que responderam não;
+        // }
 
-            
-        
-        
+
+        public double Exercise4(double[] arrayA)
+        {
+            //Dado que a aplicação está preparada. Quando o usuário chamar o exercício 4, então a aplicação deverá 
+            //receber uma matriz A do tipo vetor e Apresentar a soma de todos os elementos que sejam impares
+
+            double sumOddNumbers = 0;
+
+            foreach (var item in arrayA)
+            {
+                if (item % 2 != 0)
+                {
+                    sumOddNumbers += item;
+                }
+            }
+            return sumOddNumbers;
         }
+
+
+        public double Exercise5(double[] array)
+        {
+            //Dado que a aplicação está preparada. Quando o usuário chamar o exercício 5, então a aplicação deverá
+            //retornar quantos valores de um vetor de 10 posições são positivos
+
+            int evenNumbersCount = 0;
+            foreach (var item in array)
+            {
+                if (item % 2 == 0 && item != 0)
+                {
+                    evenNumbersCount++;
+                }
+            }
+            return evenNumbersCount;
+        }
+
+        
+        public (double, int) Exercise6(double[] array)
+        {
+            //Dado que a aplicação está preparada. Quando o usuário chamar o exercício 6, então a aplicação deverá receber
+            //um vetor de 10 números positivos e escrever o valor do maior elemento e a respectiva posição que ele ocupa no vetor.
+
+            double biggestValue = 0;
+            int index = 0;
             
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > biggestValue)
+                    {
+                        biggestValue = array[i];
+                        index = i;
+                    }
+            }
+            return (biggestValue, index);
+        }
+
+
+        public int Exercise7(double[] arrayA)
+        {
+            //Dado que a aplicação está preparada. Quando o usuário chamar o exercício 7, então a aplicação deverá receber
+            //um vetor A e imprimir na tela quantas vezes há um número residindo na mesma posição do vetor que seu valor numérico.
+
+            int indexCount = 0;
+
+            for (int i = 0; i < arrayA.Length; i++)
+            {
+                if (arrayA[i] == i)
+                {
+                    indexCount++;
+                }                       
+            }
+            return indexCount;
+        }
+
+
+        public int Exercise8(string[] arrayAlphabet)
+        {
+            //Dado que a aplicação está preparada. Quando o usuário chamar o exercício 8, então a aplicação deverá receber
+            //um vetor A e imprimir na tela quantas vezes há um número residindo na mesma posição do vetor que seu valor numérico.
+
+            int vowelCount = 0;
+
+            foreach (var item in arrayAlphabet)
+            {
+                if (item == "A" || item == "a" || item == "E" || item == "e" || item == "I" || item == "i" || item == "O" || item == "o" || item == "U" || item == "u")
+                {
+                    vowelCount++;
+                }
+            }
+            return vowelCount;
+        }
+
+
+        public string Exercise9(string[] arrayAlphabet)
+        {
+            //Dado que a aplicação está preparada. Quando o usuário chamar o exercício 9, então a aplicação deverá receber um vetor de strings de 10
+            //posições onde cada uma recebe uma letra do alfabeto. Imprima a string resultante da soma das strings que residem em índices pares
+        
+            string evenNuberIndexString = "";
+
+            for (int i = 0; i < arrayAlphabet.Length; i++)
+            {
+                if (i % 2 == 0 && i > 0)
+                {
+                    evenNuberIndexString += arrayAlphabet[i];
+                }
+            }                    
+            return evenNuberIndexString;
+        }
     }
 }
--- ArrayList2Tests.cs
+++ ArrayList2Tests.cs
@@ -34,5 +34,117 @@ namespace second_arrays_list_tests
             // Deve / Asserções
             Assert.Equal(expected, returnedValue);
         }
+
+
+        [Theory]
+        [InlineData(new double[5]{4, 22, 31, 17, 5}, 53)]
+        [InlineData(new double[3]{-5, 12, 17}, 12)]
+        [InlineData(new double[5]{13, 27, 14, 36, 11}, 51)]
+        [InlineData(new double[4]{-5, 0, 131, 40}, 126)]
+        public void Should_return_the_sum_of_odd_numbers_of_an_array(double[] arrayA, double expected)
+        {
+            // Dado / Setup
+            var exercises = new ArrayList2();
+
+            // Quando / Ação
+            double returnedValue = exercises.Exercise4(arrayA);
+
+            // Deve / Asserções
+            Assert.Equal(expected, returnedValue);
+        }
+
+
+        [Theory]
+        [InlineData(new double[5]{-10, 12, 4, 31, 67}, 3)]
+        [InlineData(new double[6]{1, 87, 96, 24, 56, 21}, 3)]
+        [InlineData(new double[10]{15, 63, 22, 48, 97, 102, 0, 69, 82, 56}, 5)]
+        [InlineData(new double[7]{4, 56, 0, -78, 93, 24, 53}, 4)]
+        public void Should_return_the_amount_of_even_numbers_in_an_array(double[] array, double expected)
+        {
+            // Dado / Setup
+            var exercises = new ArrayList2();
+
+            // Quando / Ação
+            double returnedValue = exercises.Exercise5(array);
+
+            // Deve / Asserções
+            Assert.Equal(expected, returnedValue);
+        }
+
+
+        [Theory]
+        [InlineData(new double[5]{24, 32, 16, 74, 89}, 89, 4)]
+        [InlineData(new double[6]{51, 87, 132, 54, 98, 13}, 132, 2)]
+        [InlineData(new double[7]{87, 52, 31, 49, 23, 347, 18}, 347, 5)]
+        public void should_return_the_biggest_number_in_an_array_and_its_index(double[] array, double expectedValue, int expectedIndex)
+        {
+            // Dado / Setup
+            var exercises = new ArrayList2();
+
+            // Quando / Ação
+            (double, int) returnedValue = exercises.Exercise6(array);
+
+            // Deve / Asserções
+            Assert.Equal(expectedValue, returnedValue.Item1);
+            Assert.Equal(expectedIndex, returnedValue.Item2);
+        }
+
+
+        [Theory]
+        [InlineData(new double[5]{14, 1, 5, 78, 4}, 2)]
+        [InlineData(new double[10]{0, 45, 69, 87, 22, 3, 4, 56, 8, 9}, 3)]
+        [InlineData(new double[6]{5, 6, 32, 17, 48, 9}, 0)]
+        [InlineData(new double[5]{-5, 67, 2, 34, 5}, 1)]
+        public void should_return_how_many_times_a_number_was_equal_its_index(double[] arrayA, int expectedValue)
+        {
+            // Dado / Setup
+            var exercises = new ArrayList2();
+
+            // Quando / Ação
+            int returnedValue = exercises.Exercise7(arrayA);
+
+            // Deve / Asserções
+            Assert.Equal(expectedValue, returnedValue);
+        }
+
+
+        [Theory]
+        [InlineData(new string[5]{"a", "g", "b", "e", "m"}, 2)]
+        [InlineData(new string[5]{"h", "e", "i", "o", "z"}, 3)]
+        [InlineData(new string[5]{"k", "n", "p", "r", "y"}, 0)]
+        [InlineData(new string[5]{"c", "ç", "f", "t", "u"}, 1)]
+        public void should_return_the_amount_of_vowels_in_the_array(string[] arrayAlphabet, int expectedValue)
+        {
+            // Dado / Setup
+            var exercises = new ArrayList2();
+
+            // Quando / Ação
+            int returnedValue = exercises.Exercise8(arrayAlphabet);
+
+            // Deve / Asserções
+            Assert.Equal(expectedValue, returnedValue);
+        }
+
+
+        [Theory]
+        [InlineData(new string[7]{"a", "e", "k", "l", "c", "s", "h"}, "kch")]
+        [InlineData(new string[7]{"s", "u", "m", "t", "r", "y", "e"}, "mre")]
+        [InlineData(new string[7]{"n", "j", "w", "s", "d", "r", "l"}, "wdl")]
+        [InlineData(new string[7]{"h", "v", "g", "e", "r", "ç", "f"}, "grf")]
+        public void should_return_the_result_word_with_the_even_indexes_in_the_array(string[] arrayAlphabet, string expectedValue)
+        {
+            // Dado / Setup
+            var exercises = new ArrayList2();
+
+            // Quando / Ação
+            string returnedValue = exercises.Exercise9(arrayAlphabet);
+
+            // Deve / Asserções
+            Assert.Equal(expectedValue, returnedValue);
+        }
+
+
+        [Theory]
+
     }
 }
